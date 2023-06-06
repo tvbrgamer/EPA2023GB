7dcce6f1ef2547fd666e1c6b5129b6e36bf04c31
Jonathan Aleman <tvbrgamers@gmail.com>
2023-06-06 08:43:18 -0300

    marcelo update

---
+++ Github/Site/script/script.js
@@ -0,0 +1,11 @@
+
+
+function loginPage() {
+    location.href = "loginpage.html" ;
+}
+
+/*function searchBoxClick() {
+    const searchBox = document.getElementById("searchbox");
+    searchBox.style.border = "solid 3px #BD0B08";
+}
+*/
